1872a96295f3a32a2d1ab69acd177584d146bd67
obaidulnasir <obaidulnasir@outlook.com>
2022-02-24 12:28:51 +0600

    dt to hour convert

--- src/components/Forecast/Daily/Daily.js
+++ src/components/Forecast/Daily/Daily.js
@@ -40,15 +40,17 @@ const Daily = () => {
 
   console.log(createDate(1645598754))
 
-    const dti = 1645598754;
+    const dti = 1645610400;
     
     var day = new Date(dti*1000);
     
-    console.log(day.toUTCString()) // 'Fri, 15 Jan 2021 04:32:29 GMT'
-    console.log(day.toDateString()) // 'Fri Jan 15 2021'
-    console.log(day.toISOString()) // '2021-01-15T04:32:29.000Z'
-    console.log(day.toString()) // 'Fri Jan 15 2021 07:32:29 GMT+0300 (GMT+03:00)'
+    // console.log(day.toUTCString()) // 'Fri, 15 Jan 2021 04:32:29 GMT'
+    // console.log(day.toDateString()) // 'Fri Jan 15 2021'
+    // console.log(day.toISOString()) // '2021-01-15T04:32:29.000Z'
+    // console.log(day.toString()) // 'Fri Jan 15 2021 07:32:29 GMT+0300 (GMT+03:00)'
 
+    //this Time for HOurly data
+    console.log(day.getHours(), ":", day.getMinutes()) // 13:00
  
     return (
         <div className='w-4/5 mx-auto'>
@@ -65,19 +67,19 @@ const Daily = () => {
                             <p>{it?.main?.feels_like}</p>
                         </div>
                         <div>
-                          <p>{createDate(it.dt)}</p>
+                          <p className='text-center'>{createDate(it.dt)}</p>
                             {/* <p>time: {(new Date(it?.dt * 1000 - (forecast?.timezone_offset * 1000)))}</p> */}
                         </div>
                         <div className='flex justify-between'>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                                </svg><span>{it?.main?.temp_max}</span>
+                                </svg><span>{it?.temp?.max}</span>
                             </div>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M14.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l2.293-2.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                                </svg><span>{it?.main?.temp_min}</span>
+                                </svg><span>{it?.temp?.min}</span>
                             </div>
                         </div>
                     </div>
--- src/components/Forecast/Daily/HourlyCard.js
+++ src/components/Forecast/Daily/HourlyCard.js
@@ -2,15 +2,26 @@ import React from 'react';
 import useW from '../../../hooks/useW';
 
 const HourlyCard = () => {
-    const { cityWeather } = useW();
+    const { cityWeather, forecast } = useW();
     const { list } = cityWeather;
-    const sliceList = list?.slice(0, 6);
+    const {hourly} = forecast;
+    const sliceList = hourly?.slice(0, 12);
+
+    //dti to Hour
+    // const hour = hourly[0].dt;
+    const createTime = (dt)=>{
+        const day = new Date(dt*1000);
+        return day.getHours();
+    };
+
+    
+   
 
     // console.log(forecast);
     return (
         <div className='w-4/5 mx-auto'>
             <div className='my-3'>
-                <h4 className='font-bold text-green-500 text-3xl text-center'>Next 18 Hour Expected Weather</h4>
+                <h4 className='font-bold text-green-500 text-3xl text-center'>Next 12 Hour Expected Weather</h4>
             </div>
             <div className='grid grid-cols-6 gap-4'>
                 {sliceList?.map((it) => (
@@ -19,13 +30,14 @@ const HourlyCard = () => {
                             <img src={`http://openweathermap.org/img/wn/${it?.weather[0]?.icon}@2x.png`} alt="" />
                         </div>
                         <div className='text-center text-green-500 text-lg'>
-                            <p>{it?.main?.feels_like}</p>
+                            <p>{createTime(it?.dt)}:00</p>
+                            <p>{it?.temp}&#8451;</p>
                         </div>
                         <div className='flex justify-between'>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path fillRule="evenodd" d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                                </svg><span>{it?.main?.temp_max}</span>
+                                </svg><span></span>
                             </div>
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
