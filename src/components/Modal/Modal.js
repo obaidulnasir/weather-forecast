0330538f8039783e6184d89aa411cdb7ceaf2ae8
obaidulnasir <obaidulnasir@outlook.com>
2022-02-27 11:18:48 +0600

    show date long

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,53 +5,54 @@ const Modal = (props) => {
     // const {dt}= props.dailyData;
     // console.log(dt);
     // console.log(props.it?.clouds)
-     const [modal, setModal] = useState("hidden");
-   
-    const openModal= ()=>{
+    const [modal, setModal] = useState("hidden");
+
+    const openModal = () => {
         setModal("")
         console.log("open hit")
     };
-    const closeModal = ()=> {
+    const closeModal = () => {
         setModal("hidden");
     };
 
-
-    function createDate(dt) {
-        var day = new Date(dt * 1000);
-        // if (type === "long") {
-        //     let options = {
-        //         weekday: "long",
-        //         year: "numeric",
-        //         month: "long",
-        //         day: "numeric",
-        //     };
-        //     return day.toLocaleString("en-us", options); // Friday, January 15, 2021
-        // } else {
-        return day.toLocaleString("en-us", { weekday: "long" }); // Friday
+    function createDate(dt, type) {
+        const day = new Date(dt * 1000);
+        if (type === "long") {
+            let options = {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+            };
+            return day.toLocaleString("en-us", options); // Monday, Jun 15, 2022
+        } else {
+            return day.toLocaleString("en-us", { weekday: "long" }); // Monday
+        };
     };
+
 
 
     return (
         <div >
-            <button onClick={()=>openModal()} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-700 transition" >Open modal</button>
+            <button onClick={() => openModal()} className="bg-blue-500 text-white rounded-md px-4 py-2 hover:bg-blue-700 transition" >Open modal</button>
 
             <div id="modal" className={`${modal} fixed z-50 inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4`}>
                 <div className="relative top-40 mx-auto shadow-lg rounded-md bg-white max-w-md">
 
                     {/* <!-- Modal header --> */}
                     <div className="flex justify-between items-center bg-green-600 text-white text-xl rounded-t-md px-4 py-2">
-                        <h3>{createDate(props?.it?.dt)}</h3>
-                        <button onClick={()=>closeModal()}>x</button>
+                        <h3>{createDate(props?.it?.dt, "long")}</h3>
+                        <button onClick={() => closeModal()}>x</button>
                     </div>
 
                     {/* <!-- Modal body --> */}
                     <div className="max-h-48 overflow-y-scroll p-4">
-                       {props.it.temp.max}
+                        {props.it.temp.max}
                     </div>
 
 
                     <div className="px-4 py-2 border-t border-t-gray-500 flex justify-end items-center space-x-4">
-                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition" onClick={()=>closeModal()}>Close</button>
+                        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition" onClick={() => closeModal()}>Close</button>
                     </div>
                 </div>
             </div>
